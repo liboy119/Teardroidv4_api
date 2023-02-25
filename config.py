ee2cb4088aa283f28197f823ab18c31ea17ba967
liboy119 <89219487+liboy119@users.noreply.github.com>
2023-02-25 18:33:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-USER_AGENT = "hello"
+USER_AGENT = "Mozilla/5.0 (Linux; Android 8.1.0; vivo Y83A Build/O11019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36 VivoBrowser/13.6.0.0"
